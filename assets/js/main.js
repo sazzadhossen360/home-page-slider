d286f37a326843b62315ade6e07d286d5e506be5
sazzad hossen <sazzadhossen2544@gmail.com>
2020-05-10 18:08:11 +0600

    initiated git repo

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+    jQuery(document).ready(function($){
+        $(document).ready(function(){
+            $('.owl-carousel').owlCarousel({
+                items:1,
+                autoplay:true,
+                margin:30,
+                dots:true,
+                navs:true,
+                loop:true
+            });
+            
+          });
+    });
